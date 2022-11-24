e8441cf2982f0902d91f85e137ce2547f61ae01d
Fred Lahode <lahode@infomaniak.ch>
2022-11-24 22:27:42 +0100

    Upgrade project

--- tests/9/tests.sh
+++ tests/9/tests.sh
@@ -22,20 +22,21 @@ DB_URL="${DB_DRIVER}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}"
 
 make init -f /usr/local/bin/actions.mk
 
-composer -vvv require -n drupal/redis
-composer -vvv require -n drupal/search_api
-composer -vvv require -n drupal/search_api_solr
-composer -vvv require -n drupal/purge
-composer -vvv require -n drupal/varnish_purge
-#    does not support Drupal 9 yet
-#composer require -n drupal/cache_tags
+composer require -n \
+  drupal/redis \
+  drupal/purge \
+  drupal/varnish_purge
+
+composer require -n \
+  drupal/search_api \
+  drupal/search_api_solr:~4
 
 cd ./web
 
 drush si -y --db-url="${DB_URL}"
 
 # Test Drupal status and requirements
-check_status "drush-version" "10.*"
+check_status "drush-version" "11.*"
 check_status "root" "${APP_ROOT}/${DOCROOT_SUBDIR}"
 check_status "site" "sites/default"
 check_status "files" "sites/default/files"
@@ -50,15 +51,13 @@ check_rq "Configuration files" "Protected"
 
 drush en -y \
   redis \
-  search_api \
-  search_api_solr \
   purge \
   purge_queuer_coretags \
   purge_drush \
   varnish_purger \
   varnish_purge_tags
-#  \
-#  cache_tags
+
+drush en -y search_api_solr_admin
 
 # Enable redis
 chmod 755 "${PWD}/sites/default/settings.php"
@@ -67,33 +66,31 @@ check_rq "Redis" "Connected"
 
 check_rq "Trusted Host Settings" "Enabled"
 
-# @todo enabled when drupal console will be installed.
 # Import solr server
-#drupal cis --file search_api.server.solr.yml --directory /var/www/html/web
-#drush sapi-sl | grep -q enabled
-
-# @TODO return varnish tests after purge module drush commands support drush 9
+drush cim --source=/var/www/html/solr --partial -y
+drush solr-upload-conf solr 1
+drush sapi-sl | grep -q enabled
 
 ## Test varnish cache and purge
-#cp varnish-purger.yml purger.yml
-#
-#drush ppadd varnish
-#drush cr
+drush ppadd varnish
+drush cr
 
 ## Workaround for varnish purger import https://www.drupal.org/node/2856221
-#PURGER_ID=$(drush ppls --format=json | jq -r "keys[0]")
-#
-#sed -i "s/PLUGIN_ID/${PURGER_ID}/g" purger.yml
-#mv purger.yml "varnish_purger.settings.${PURGER_ID}.yml"
-#drupal cis --file "varnish_purger.settings.${PURGER_ID}.yml"
-#
-#drush -y config-set system.performance cache.page.max_age 43200
-#
-#curl -Is varnish:6081 | grep -q "X-Varnish-Cache: MISS"
-#curl -Is varnish:6081 | grep -q "X-Varnish-Cache: HIT"
-#
-#drush cc render
-#drush pqw
-#
-#curl -Is varnish:6081 | grep -q "X-Varnish-Cache: MISS"
-#curl -Is varnish:6081 | grep -q "X-Varnish-Cache: HIT"
+PURGER_ID=$(drush ppls --format=json | jq -r "keys[0]")
+
+mkdir -p /var/www/html/varnish
+# We copy mounted file because we can't edit mounted file (resource busy error).
+cp /var/www/html/varnish-purger.yml /var/www/html/varnish/purger.yml
+sed -i "s/PLUGIN_ID/${PURGER_ID}/g" /var/www/html/varnish/purger.yml
+mv /var/www/html/varnish/purger.yml "/var/www/html/varnish/varnish_purger.settings.${PURGER_ID}.yml"
+drush -y cim --source=/var/www/html/varnish --partial
+drush -y config-set system.performance cache.page.max_age 43200
+
+curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: MISS"
+curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: HIT"
+
+drush cc render
+drush pqw
+
+curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: MISS"
+curl -Is -H 'Host: drupal.localhost' varnish:6081 | grep -q "X-VC-Cache: HIT"
